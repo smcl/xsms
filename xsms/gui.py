6b036295d04f6109d0652a9841d11ef7d5e585a8
smcl <sean.mclemon@gmail.com>
2016-10-25 19:24:17 +0200

    use print the date

--- xsms/gui.py
+++ xsms/gui.py
@@ -7,10 +7,12 @@ def derp(messages):
     r = 0
     for m in messages:
         Label(header, text=m.sender, anchor="w").grid(row=r,column=0, sticky=W)
-        Label(header, text="2016-10-23 12:01", anchor="e").grid(row=r,column=1)
+        Label(header, text=m.date_received, anchor="e").grid(row=r,column=1)
         msg = Text(header, height=4, width=35)
         msg.grid(row=r+1, column=0, columnspan=2)
         msg.insert("1.0", m.message)
+        msg.config(state=DISABLED)
+
         r = r + 2
 
     header.pack()
