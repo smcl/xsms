bf0f02737af8c0cd2d4aab7660b69c9f686ed34a
smcl <sean.mclemon@gmail.com>
2016-10-27 21:25:33 +0200

    compose and outbox now working

--- xsms/__main__.py
+++ xsms/__main__.py
@@ -20,17 +20,18 @@ from .gui import launch_gui
 # system imports
 import argparse
 
+# there's gotta be a better way than a global...
+modem = None
+
 def get_args():
     parser = argparse.ArgumentParser(
         description="xsms - an sms client for linux systems with an em73xx modem",
         # epilog="some bullshit example usage"
     )
 
-    # required switch
-    parser.add_argument('--device', action='store', required=True)
-
-    # optional switch
-    parser.add_argument('--check', action='store_true')
+    # optional switches
+    parser.add_argument('--device', action='store')
+    parser.add_argument('--gui', action='store_true')
 
     # optional args
     parser.add_argument('--pin', action='store', default="")
@@ -40,7 +41,7 @@ def get_args():
     return parser.parse_args()
 
 
-def check_only(read_format, unread_format, messages):
+def print_unread_messages(read_format, unread_format, messages):
     unread_count = len([m for m in messages if not m.read])
 
     if unread_count:
@@ -52,25 +53,26 @@ def check_only(read_format, unread_format, messages):
 if __name__ == '__main__':
     args = get_args()
 
-    # initialise the device
-    modem = Modem(args.device, pin=args.pin)
-
     # read existing inbox and outbox
     inbox_messages = inbox.read()
     outbox_messages = outbox.read()
 
     # retrieve new messages from the modem and add to the inbox
-    for m in modem.getSMS():
-        inbox_messages.append(m)
+    if args.device:
+        # initialise the device
+        modem = Modem(args.device, pin=args.pin)
 
-    # serialise inbox
-    inbox.write(inbox_messages)
+        for m in modem.getSMS():
+            inbox_messages.append(m)
 
-    # clear outstanding messages from SIM
-    modem.deleteAllSMS()
+            # serialise inbox
+            inbox.write(inbox_messages)
+
+            # clear outstanding messages from SIM
+            modem.deleteAllSMS()
 
     # decide whether to launch the GUI or just check + print unread
-    if args.check:
-        check_only(args.read_format, args.unread_format, inbox_messages)
+    if args.gui:
+        launch_gui(inbox_messages, outbox_messages, modem)
     else:
-        launch_gui(inbox_messages, outbox_messages)
+        print_unread_messages(args.read_format, args.unread_format, inbox_messages)
--- xsms/gui.py
+++ xsms/gui.py
@@ -1,7 +1,9 @@
 # flake8: noqa
 import ttk
 from Tkinter import *
-
+from datetime import datetime
+from em73xx import SMS
+import outbox
 
 def mailbox_frame(parent, messages):
     f = ttk.Frame(parent)
@@ -27,12 +29,39 @@ def create_outbox_frame(parent, messages):
     return mailbox_frame(parent, messages)
 
 
-def create_compose_frame(parent):
+# this is some nasty shit
+def sendmsg(recipient, message, modem):
+    modem.sendSMS(str(recipient), str(message))
+    outbox_messages = outbox.read()
+    outbox_messages.append(
+        SMS(-1, None, recipient, datetime.now(), message, True)
+    )
+    outbox.write(outbox_messages)
+
+def create_compose_frame(parent, modem):
     f = ttk.Frame(parent)
+
+    recipient_input = Entry(f, width=35)
+    message_input = Text(f, height=4, width=35)
+    send_button = Button(f, text="Send", command=lambda: sendmsg(recipient_input.get(), message_input.get("1.0", END), modem))
+
+    # if we haven't initialised a device, we can't send anything
+    if not modem:
+        message_input.insert("1.0", "No modem initialised, start xsms with the --device switch if you want to send messages")
+        recipient_input.config(state=DISABLED)
+        message_input.config(state=DISABLED)
+        send_button.config(state=DISABLED)
+
+
+
+    recipient_input.pack(fill=X)
+    message_input.pack(fill=X)
+    send_button.pack()
+
     return f
 
 
-def launch_gui(inbox_messages, outbox_messages):
+def launch_gui(inbox_messages, outbox_messages, modem):
     root = Tk()
     root.title("xsms")
 
@@ -40,7 +69,7 @@ def launch_gui(inbox_messages, outbox_messages):
 
     inbox_frame = create_inbox_frame(nb, inbox_messages)
     outbox_frame = create_outbox_frame(nb, outbox_messages)
-    compose_frame = create_compose_frame(nb)
+    compose_frame = create_compose_frame(nb, modem)
 
     nb.add(inbox_frame, text="Inbox")
     nb.add(outbox_frame, text="Sent")
