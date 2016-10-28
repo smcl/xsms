7df91b1d44f9e848f5937c3d40d19917844bd2ca
smcl <sean.mclemon@gmail.com>
2016-10-28 21:02:36 +0200

    UI is a little more object-y

--- xsms/gui.py
+++ xsms/gui.py
@@ -6,80 +6,102 @@ from em73xx import SMS
 import outbox
 import style
 
-def mailbox_frame(parent, messages):
-    f = ttk.Frame(parent)
 
-    r = 0
-    for m in messages:
-        ttk.Label(f, text=m.sender, anchor="w").grid(row=r,column=0, sticky=Tkinter.W)
-        ttk.Label(f, text=m.date_received.strftime('%d/%m/%Y %H:%M'), anchor="e").grid(row=r,column=1)
-        msg = style.Text(f, 4, 35)
-        msg.grid(row=r+1, column=0, columnspan=2)
-        msg.insert("1.0", m.message)
-        msg.config(state=Tkinter.DISABLED)
+class Gui(object):
 
-        r = r + 2
+    def __init__(self, modem):
+        self.modem = modem
 
-    return f
+    def show(self, inbox_messages, outbox_messages):
+        root = Tkinter.Tk()
+        root.title("xsms")
+        nb = ttk.Notebook(root)
 
-def create_inbox_frame(parent, messages):
-    return mailbox_frame(parent, messages)
+        #style.configure(ttk.Style())
+        #ttk.Style().theme_use("classic")
 
+        # create frames
+        self.inbox_frame = self.create_inbox_frame(nb, inbox_messages)
+        self.outbox_frame = self.create_outbox_frame(nb, outbox_messages)
+        self.compose_frame = self.create_compose_frame(nb)
 
-def create_outbox_frame(parent, messages):
-    return mailbox_frame(parent, messages)
+        # add frames to the notebook
+        nb.add(self.inbox_frame, text="Inbox")
+        nb.add(self.outbox_frame, text="Sent")
+        nb.add(self.compose_frame, text="Compose")
 
+        # render the main window
+        nb.pack(expand=1, fill="both")
+        root.mainloop()
 
-# this is some nasty shit
-def sendmsg(recipient, message, modem):
-    if not recipient.strip() or not message.strip():
-        return
+    def populate_messages(self, f, messages):
+        r = 0
+        for m in messages:
+            ttk.Label(f, text=m.sender, anchor="w").grid(row=r,column=0, sticky=Tkinter.W)
+            ttk.Label(f, text=m.date_received.strftime('%d/%m/%Y %H:%M'), anchor="e").grid(row=r,column=1)
+            msg = style.Text(f, 4, 35)
+            msg.grid(row=r+1, column=0, columnspan=2)
+            msg.insert("1.0", m.message)
+            msg.config(state=Tkinter.DISABLED)
 
-    modem.sendSMS(str(recipient), str(message))
-    outbox_messages = outbox.read()
-    outbox_messages.append(
-        SMS(-1, None, recipient, datetime.now(), message, True)
-    )
-    outbox.write(outbox_messages)
+            r = r + 2
 
-def create_compose_frame(parent, modem):
-    f = ttk.Frame(parent)
+    def mailbox_frame(self, parent, messages):
+        f = ttk.Frame(parent)
+        self.populate_messages(f, messages)
+        return f
 
-    recipient_input = ttk.Entry(f, width=30)
-    message_input = style.Text(f, 4, 30)
-    send_button = ttk.Button(f, text="Send", command=lambda: sendmsg(recipient_input.get(), message_input.get("1.0", END), modem))
+    def create_inbox_frame(self, parent, messages):
+        return self.mailbox_frame(parent, messages)
 
-    # if we haven't initialised a device, we can't send anything
-    if not modem:
-        message_input.insert("1.0", "No modem initialised, start xsms with the --device switch if you want to send messages")
-        recipient_input.config(state=Tkinter.DISABLED)
-        message_input.config(state=Tkinter.DISABLED)
-        send_button.config(state=Tkinter.DISABLED)
+    def create_outbox_frame(self, parent, messages):
+        return self.mailbox_frame(parent, messages)
 
-    recipient_input.pack(fill=Tkinter.X)
-    message_input.pack(fill=Tkinter.X)
-    send_button.pack(fill=Tkinter.X)
+    def create_compose_frame(self, parent):
+        f = ttk.Frame(parent)
 
-    return f
+        self.recipient_input = ttk.Entry(f, width=30)
+        self.message_input = style.Text(f, 4, 30)
+        self.send_button = ttk.Button(f, text="Send", command=lambda: self.send(self.recipient_input.get(), self.message_input.get("1.0", Tkinter.END)))
+
+        # if we haven't initialised a device, we can't send anything
+        if not self.modem:
+            self.message_input.insert("1.0", "No modem initialised, start xsms with the --device switch if you want to send messages")
+            self.recipient_input.config(state=Tkinter.DISABLED)
+            self.message_input.config(state=Tkinter.DISABLED)
+            self.send_button.config(state=Tkinter.DISABLED)
+
+        self.recipient_input.pack(fill=Tkinter.X)
+        self.message_input.pack(fill=Tkinter.X)
+        self.send_button.pack(fill=Tkinter.X)
+
+        return f
+
+    def send(self, recipient, message):
+        if not recipient.strip() or not message.strip():
+            return
+
+        self.modem.sendSMS(str(recipient), str(message))
+
+        # I know this ain't pretty
+        outbox.append(
+            SMS(-1, None, recipient, datetime.now(), message, True)
+        )
+
+        # clear the outbox frame
+        for widget in self.outbox_frame.winfo_children():
+            widget.destroy()
+
+        # and refresh it with the new inbox
+        self.populate_messages(self.outbox_frame, outbox.read())
+
+        # clear the compose frame
+        self.message_input.delete("1.0", Tkinter.END)
+        self.message_input.insert("1.0", "")
+        self.recipient_input.delete(0, Tkinter.END)
+        self.recipient_input.insert(0, "")
 
 
 def launch_gui(inbox_messages, outbox_messages, modem):
-    root = Tkinter.Tk()
-    root.title("xsms")
-
-    nb = ttk.Notebook(root)
-
-    #style.configure(ttk.Style())
-    #ttk.Style().theme_use("classic")
-
-    inbox_frame = create_inbox_frame(nb, inbox_messages)
-    outbox_frame = create_outbox_frame(nb, outbox_messages)
-    compose_frame = create_compose_frame(nb, modem)
-
-    nb.add(inbox_frame, text="Inbox")
-    nb.add(outbox_frame, text="Sent")
-    nb.add(compose_frame, text="Compose")
-
-    nb.pack(expand=1, fill="both")
-
-    root.mainloop()
+    ui = Gui(modem)
+    ui.show(inbox_messages, outbox_messages)
--- xsms/outbox.py
+++ xsms/outbox.py
@@ -9,5 +9,11 @@ def read():
     return read_messages(outbox_filename)
 
 
+def append(message):
+    new_outbox = read()
+    new_outbox.append(message)
+    write(new_outbox)
+
+
 def write(messages):
     write_messages(outbox_filename, messages)
